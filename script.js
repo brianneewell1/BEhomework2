519ff155aa773775f3a585c1ebf68ccea910eb80
Brianne <brianne.ewell1@gmail.com>
2020-11-05 21:25:51 -0700

    added linkedin and github links

---
+++ script.js
@@ -0,0 +1,6 @@
+var gitHub = document.getElementById("gitHub");
+var linkedIn = document.getElementById("linkedIn");
+
+function gitHub() {
+    onclick="location.href = 'https://github.com/brianneewell1'";
+}
